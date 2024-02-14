469e1a47447bf1fdcfc5671b06e46df6ac82618a
Red0220 <stevenkin817@gmail.com>
2024-02-14 17:49:10 +0100

    mern-blog

---
+++ client/vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  server: {
+    port: 5173,
+    overlay: false,
+  },
+  plugins: [react()],
+})
