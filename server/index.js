bb2869b7af6d5da551ee1e30ea9ffd90e07b4e8c
Red0220 <stevenkin817@gmail.com>
2024-02-16 18:02:13 +0100

    sign up

--- server/index.js
+++ server/index.js
@@ -1,16 +1,19 @@
 import mongoose from 'mongoose';
 import express from 'express';
 import dotenv from 'dotenv';
-import userRoutes from './routes/user.route.js'
+import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js'
 
 dotenv.config();
 const app = express();
+app.use(express.json());
 
 mongoose.connect(process.env.MONGOOSE_URL)
 .then(()=> console.log('connected ...'))
 .catch(()=> console.log(error));
 
-app.use('/server/user',userRoutes)
+app.use('/server/user',userRoutes);
+app.use('/server/auth',authRoutes);
 
 
 app.listen(3000, ()=> console.log('listening ... !'))
